0f4afcc6b7aafe3b7eee34dca32ec66e10961c52
Nick Noel <nicknoel251289@gmail.com>
2018-03-01 02:56:43 -0500

    updated bio and added a smooth scroll

--- Scripts/index.js
+++ Scripts/index.js
@@ -23,4 +23,21 @@ $(document).ready(function(){
         $('.art-grid').css('opacity', '0');
       }
   });
+
+  $(".btn").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+
 });
